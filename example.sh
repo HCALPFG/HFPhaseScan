a18d7c982b6e07f20699d78d407fdc2a4e53dc44
Edmund A Berry <Edmund.A.Berry@CERN.CH>
2015-05-12 15:15:40 +0200

    Initial commit

---
+++ example.sh
@@ -0,0 +1,6 @@
+python make_points.py \
+       -it data/default/TTCrxPhaseDelayRBX_0x187_tunedHTRs_tuned_v11m.xml \
+       -ib data/delays_HBHE \
+       -ot data/hbhe_shiftM5/ttcrx.xml \
+       -ob data/hbhe_shiftM5/delay \
+       -s  -5
