496d094b624305e1266f9be4b6b08d833dc87e98
lucien1011 <lucien1011@gmail.com>
2015-05-29 11:50:26 +0200

    updated files

--- DelayByChannel/Configuration.py
+++ DelayByChannel/Configuration.py
@@ -6,11 +6,16 @@
 
 def delaySettingFiles():
 	return {
-		"depth1":("Data/test_depth1.root","shift_depth1"),
-		"depth2":("Data/test_depth2.root","shift_depth2"),
+		"depth1":("Data/20150528_RelativeTimeShift.root","shift_depth1"),
+		"depth2":("Data/20150528_RelativeTimeShift.root","shift_depth2"),
 
 		}
 
 def mapFile():
 	return "Data/emap.txt"
 
+def brickDir():
+	return "/afs/cern.ch/work/k/klo/hcal/PromptAnalysis/HFPhaseScan/DelayByChannel/Data/2015-may-12"
+
+def TTCPath():
+	return "/afs/cern.ch/work/k/klo/hcal/PromptAnalysis/HFPhaseScan/DelayByChannel/Data/TTCrxPhaseDelayRBX_0x187_tunedHTRs_tuned_v13m.xml"
