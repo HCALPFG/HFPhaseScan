ba567fd4f9118080d4b0a7c39694f97e26845be6
lucien1011 <lucien1011@gmail.com>
2015-05-29 11:51:20 +0200

    delay setting now adjusted according to input

--- DelayByChannel/HFSetting.py
+++ DelayByChannel/HFSetting.py
@@ -4,9 +4,10 @@ class HFSetting(object):
 	def __init__(self,mapFileName):
 		self._file = open(mapFileName)
 
-		self._setting = {}
+		self._EtaPhiSetting = {}
 		self._map = {}
 		self._RBXSetting = {}
+		self._chSetting = {}
 
 		for line in self._file:
 			if line[0] == "#": continue
@@ -16,27 +17,36 @@ class HFSetting(object):
 			iphi = int(fieldList[3])
 			depth = int(fieldList[5])
 			subDet = fieldList[7][:3]
-			self._setting[(ieta,iphi,depth,subDet)] = ""
+			self._EtaPhiSetting[(ieta,iphi,depth,subDet)] = ""
 			rbx = fieldList[7]
 			qie = int(fieldList[12])
 			rm = int(fieldList[9])
 			cand = int(fieldList[11])
-			self._map[(ieta,iphi,depth,subDet)] = deepcopy((rbx,qie,rm,cand))
-			self._RBXSetting[(rbx,qie,rm,cand)] = ""
+			self._map[(ieta,iphi,depth,subDet)] = (rbx,qie,rm,cand)
+			self._RBXSetting[rbx] = ""
+			self._chSetting[(rbx,qie,rm,cand)] = ""
 
 	
-	def setValue(self,value,ieta,iphi,depth,subDet):
-		if (ieta,iphi,depth,subDet) in self._setting:
+	def setValueWithEtaPhi(self,value,ieta,iphi,depth,subDet):
+		if (ieta,iphi,depth,subDet) in self._EtaPhiSetting:
 			self._setting[(ieta,iphi,depth,subDet)] = value
-			self._RBXSetting[self.getRBXCoords(ieta,iphi,depth,subDet)] = value
 		else:
 			raise RuntimeError,"Can't find (ieta,iphi,depth,subDet) in the emap for HF"
 	
-	def __iter__(self):
-		return iter(self._RBXSetting)
+	def setValueWithCH(self,value,rbx,qie,rm,cand):
+		if (rbx,qie,rm,cand) in self._chSetting:
+			self._chSetting[(rbx,qie,rm,cand)] = value
+		else:
+			raise RuntimError,"Can't find (rbx,qie,rm,cand) in the emap for HF"
 
-	def getValue(self,ieta,iphi,depth,subDet):
-		return self._setting[(ieta,iphi,depth,subDet)]
+	def setValueWithRBX(self,value,rbx):
+		if rbx in self._RBXSetting:
+			self._RBXSetting[rbx] = value
+		else:
+			raise RuntimeError,"Can't find RBX in the emap for HF"
+	
+	def __iter__(self):
+		return iter(self._chSetting)
 
 	def cleanUp(self):
 		self._file.close()
@@ -48,4 +58,10 @@ class HFSetting(object):
 			raise RuntimeError,"Can't find (ieta,iphi,depth,subDet) in the emap for HF"
 
 	def getRBXValue(self,coord):
-		return self._RBXSetting[coord]
+		return self._chSetting[coord]
+	
+	def getChDelaySetting(self):
+		return self._chSetting
+
+	def getRBXDelaySetting(self):
+		return self._RBXSetting
